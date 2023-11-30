4721ed9f756866914423e582573318dbb5e6e9ae
いっしき <88958917+KellyCu@users.noreply.github.com>
2023-11-30 16:27:38 +0800

    v 0.1.2
    back end is 70% complete still need designs for front end

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const session  = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const malScraper = require('mal-scraper');
 const app = express();
+const path = require('path'); 
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+  secret: 'gensco', // Change this to a secure, random value
+  resave: false,
+  saveUninitialized: true
+}));
 
 const search = malScraper.search
 // const type = 'anime'
@@ -33,52 +40,116 @@ const search = malScraper.search
 //       year: 2015
 //     },
 //     genreType: 0, // 0 for include genre list
-//     genres: [8] // 8 corresponds to the Drama genre, adjust as needed
+//     genres: [1, 4, 8] // 8 corresponds to the Drama genre, adjust as needed
 //   })
 //     .then(console.log)
 //     .catch(console.error);
 
+
+
+
+// FUNCTIONS ----------------------------------------------------------------
+function convertGenresToCodes(sessGenre) {
+  const genreMapping = {
+    'Action': 1,
+    'Adventure': 2,
+    'Comedy': 4,
+    'Drama': 8,
+    'Ecchi': 9,
+    'Fantasy': 10,
+    'Game': 11,
+    'Harem': 13,
+    'Horror': 14,
+    'Mecha': 18,
+    'Music': 19,
+    'Mystery': 7,
+    'Psychological': 40,
+    'Romance': 22,
+    'Sci-Fi': 24,
+    'Slice of Life': 36,
+    'Sports': 30,
+    'Supernatural': 37,
+    'Thriller': 41
+  };
+
+  // Use map to convert each genre name to its corresponding code
+  const genreCodes = sessGenre.map(genre => genreMapping[genre]);
+
+  return genreCodes;
+}
+
+// --------------------------------------------------------------------------------------
+
+
 app.post("/search", (req, res) => {
-    const term = req.body.usrinp;
-    const type = 'anime';
-    search.search(term, {
-        maxResults: 10, // Adjust as needed
-        term: term,
-        type: 0,
-        status: 0,
-        score: 0,
-        producer: 0,
-        rating: 0,
-        startDate: {
-          day: 12,
-          month: 2,
-          year: 1990
-        },
-        endDate: {
-          day: 12,
-          month: 2,
-          year: 2015
-        },
-        genreType: 0, // 0 for include genre list
-        genres: [0] // 8 corresponds to the Drama genre, adjust as needed
-      })
-        .then(console.log)
-        .catch(console.error);
+  const aniTitle = req.body.usrinp;
+
+  malScraper.getInfoFromName(aniTitle)
+    .then((data) => {
+      // Extract the desired information
+      const { genres, score } = data;
+
+      req.session.genres = genres;
+      req.session.score = score;
+
+      // Log or send the extracted information
+      console.log("Genres:", genres);
+      console.log("Score:", score);
+
+      // Send a response with the extracted information
+      // res.status(200).json({
+      //   genres: genres,
+      //   score: score
+      // });
+      res.redirect('/recommendations');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 });
 
 
+app.get('/recommendations', async (req, res) => {
+  try {
+    res.sendFile(path.join(__dirname, 'public', 'html', 'Reco.html'));
+
+    const sessGenre = req.session.genres || [];
+    const genreCodes = convertGenresToCodes(sessGenre);
+    // const sessScore = Number(req.session.score) || 0;
+
+    // console.log("Debug - Search Criteria:", {
+    //   maxResults: 10,
+    //   term: 0,
+    //   type: 0,
+    //   status: 0,
+    //   score: sessScore,
+    //   genreType: 0,
+    //   genres: genreCodes
+    // });
+    
+
+    var type = 'anime';
+    const searchResults = await search.search(type, {
+        maxResults: 10,
+        term: 0,
+        type: 0,
+        status: 0,
+        score: 0,
+        genreType: 0,
+        genres: genreCodes
+    });
+    req.session.sessResults = searchResults;
+    const sessRes = req.session.sessResults;
+    console.log("DEBUG SESSION RESULTS:");
+    console.log(sessRes);
 
 
-
-
-
-
-
-
-
-
-
-
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.get("/", (req, res) => {
     res.set({
